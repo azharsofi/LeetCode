92b40543f609f53eebfd39cb83b025895ebf4d30
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-01-19 12:36:14 +0530

    Create reversestring.cpp

---
+++ reversestring.cpp
@@ -0,0 +1,15 @@
+lass Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        
+        int st=0,e=n-1;
+        while(st<e){
+            swap(s[st],s[e]);
+            st++;
+            e--;
+
+        }
+        
+    }
+};
