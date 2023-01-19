5cf670f031919955c43255162ad01e9e89c9160e
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-01-19 12:52:13 +0530

    Create perfectnumber.cpp

---
+++ perfectnumber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int s=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0)
+            s=s+i;
+        
+        }
+        if(s==num)
+        return true;
+        return false;
+        
+    }
+};
