5faa1cdb36a0951c70951e2d7c37b41eaabafcbc
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-01-22 13:43:50 +0530

    Create romantoint.cpp

---
+++ romantoint.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char,int> map={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+};
+int n=s.size();
+int total=map[s.back()];
+for(int i=n-2;i>=0;i--){
+    if(map[s[i]]<map[s[i+1]])
+    {
+        total=total-map[s[i]];
+    }
+    else
+    total+=map[s[i]];
+}
+   return total;     
+    }
+};
