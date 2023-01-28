88a687b9a56fec8f84c3b8466faa3752ca0dc04f
MOHAMMAD AZHAR SOFI <98463584+azharsofi@users.noreply.github.com>
2023-01-28 16:52:53 +0530

    Create minimumabsolutediff.cpp

---
+++ minimumabsolutediff.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+         vector<vector<int>> a;
+         
+
+         int n=arr.size();
+         sort(arr.begin(),arr.end());
+         
+            int min=INT_MAX;
+          for(int i=0;i<n-1;i++){
+             if(abs(arr[i]-arr[i+1])<=min)
+             min=abs(arr[i]-arr[i+1]);
+             }
+         for(int i=0;i<n-1;i++){
+             if(abs(arr[i]-arr[i+1])<=min){
+             min=abs(arr[i]-arr[i+1]);
+             a.push_back({arr[i],arr[i+1]});}
+         }
+        
+        return a;
+    }
+};
